9ea89d8710974c79adfcb81f7d2dda9ae78eaeea
jeverlyro <ikadektresnajeverly78@gmail.com>
2025-05-06 16:56:42 +0800

    refactor: clean up comments and improve readability in app, authController, and authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,19 +4,15 @@ const userRepository = require("../repositories/userRepository");
 const protect = async (req, res, next) => {
   let token;
 
-  // Check if authorization header exists and has the Bearer format
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
-      // Get token from header
       token = req.headers.authorization.split(" ")[1];
 
-      // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Get user from the token
       req.user = await userRepository.findUserById(decoded.id);
 
       next();
